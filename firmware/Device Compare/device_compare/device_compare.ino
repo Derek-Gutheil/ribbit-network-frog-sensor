d1bef798acb5b22b6206be22db64d63d2b325286
Derek-Gutheil <43013944+Derek-Gutheil@users.noreply.github.com>
2022-04-12 01:18:03 +0200

    Update device_compare.ino
    Lots of updates to get 3 devices to report their average temperature and pressure every 5 minutes

--- firmware/Device Compare/device_compare/device_compare.ino
+++ firmware/Device Compare/device_compare/device_compare.ino
@@ -1,12 +1,20 @@
 // Test sketch for comparing devices
 
+
+#define DEVICE_TYPE "Ribbit Embedded 0.1"
+#define DEVICE_NUMBER "1"
+#define REST_TIME (15) //Rest time between reading from the sensors in seconds
+#define REST_TIME_MS (REST_TIME * 1000)
+#define POST_TIME (300) //time between posts to the internet in seconds
+#define NUM_LOOPS (POST_TIME / REST_TIME) //how many loops do we need to run before posting?
+
 // ---------------------------------------------------------------------------------------------------------------
 // ------------------------------------------- Wifi and InfluxDB -------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------
 #if defined(ESP32)
 #include <WiFiMulti.h>
 WiFiMulti wifiMulti;
-#define DEVICE "ESP32"
+
 #elif defined(ESP8266)
 #include <ESP8266WiFiMulti.h>
 ESP8266WiFiMulti wifiMulti;
@@ -17,9 +25,9 @@ ESP8266WiFiMulti wifiMulti;
 #include <InfluxDbCloud.h>
 
 // WiFi AP SSID
-#define WIFI_SSID "**********"
+#define WIFI_SSID "*******"
 // WiFi password
-#define WIFI_PASSWORD "***************"
+#define WIFI_PASSWORD "********"
 // InfluxDB v2 server url, e.g. https://eu-central-1-1.aws.cloud2.influxdata.com (Use: InfluxDB UI -> Load Data -> Client Libraries)
 #define INFLUXDB_URL "https://westeurope-1.azure.cloud2.influxdata.com"
 // InfluxDB v2 server or cloud API token (Use: InfluxDB UI -> Data -> API Tokens -> Generate API Token)
@@ -48,39 +56,70 @@ Point sensor("wifi_status");
 // ---------------------------------------------------------------------------------------------------------------
 // ------------------------------------ Pressure and Temperature -------------------------------------------------
 // ---------------------------------------------------------------------------------------------------------------
-#include <Adafruit_DPS310.h>
+#include <Dps310.h>
 
-Adafruit_DPS310 dps;
+#define PASCAL_TO_MILLIBAR (0.01)
 
+// Dps310 Opject
+Dps310 Dps310PressureSensor = Dps310();
 
+float averageTemperatureArray[NUM_LOOPS];
+float averagePressureArray[NUM_LOOPS];
+float averageTemperature;
+float averagePressure;
 
-
-
-
-
-
+uint8_t loopCounter;
 
 
 void setup() {
+
+  //turn off bluetooth
+  btStop();
+  
+  loopCounter = 0u;
+  averageTemperature = 0.0;
+  averagePressure = 0.0;
+
+  for(int i = 0; i < NUM_LOOPS; i++)
+  {
+    averageTemperatureArray[i] = 0.0;
+    averagePressureArray[i] = 0.0;
+  }
+  
   Serial.begin(115200);
-
-
 
   // ---------------------------------------------------------------------------------------------------------------
   // ------------------------------------ Pressure and Temperature -------------------------------------------------
   // ---------------------------------------------------------------------------------------------------------------
   Serial.println("DPS310");
-  if (! dps.begin_I2C()) {             // Can pass in I2C address here
-  //if (! dps.begin_SPI(DPS310_CS)) {  // If you want to use SPI
-    Serial.println("Failed to find DPS");
-    while (1) yield();
+  Dps310PressureSensor.begin(Wire);
+
+  //temperature measure rate (value from 0 to 7)
+  //2^temp_mr temperature measurement results per second
+  int16_t temp_mr = 0;
+  //temperature oversampling rate (value from 0 to 7)
+  //2^temp_osr internal temperature measurements per result
+  //A higher value increases precision
+  int16_t temp_osr = 0;
+  //pressure measure rate (value from 0 to 7)
+  //2^prs_mr pressure measurement results per second
+  int16_t prs_mr = 0;
+  //pressure oversampling rate (value from 0 to 7)
+  //2^prs_osr internal pressure measurements per result
+  //A higher value increases precision
+  int16_t prs_osr = 0;
+  int16_t ret = Dps310PressureSensor.startMeasureBothCont(temp_mr, temp_osr, prs_mr, prs_osr);
+
+  //This setup should get us 1 measurement per second with no oversample
+  if (ret != 0)
+  {
+    Serial.print("DPS310 Init FAILED! ret = ");
+    Serial.println(ret);
   }
-  Serial.println("DPS OK!");
-  dps.configurePressure(DPS310_64HZ, DPS310_64SAMPLES);
-  dps.configureTemperature(DPS310_64HZ, DPS310_64SAMPLES);
-
-
-
+  else
+  {
+    Serial.println("DPS310 Init complete!");
+  }
 
 
   // ---------------------------------------------------------------------------------------------------------------
@@ -99,8 +138,8 @@ void setup() {
   Serial.println();
 
   // Add tags
-  sensor.addTag("device", DEVICE);
-  sensor.addTag("SSID", WiFi.SSID());
+  sensor.addTag("device_type", DEVICE_TYPE);
+  sensor.addTag("device_number", DEVICE_NUMBER);
 
   // Accurate time is necessary for certificate validation and writing in batches
   // For the fastest time sync find NTP servers in your area: https://www.pool.ntp.org/zone/
@@ -122,61 +161,166 @@ void setup() {
 
 void loop() 
 {
-
+  
   // ---------------------------------------------------------------------------------------------------------------
   // ------------------------------------ Pressure and Temperature -------------------------------------------------
   // ---------------------------------------------------------------------------------------------------------------
-  sensors_event_t temp_event, pressure_event;
+  uint8_t pressureCount = 32;
+  float pressure[pressureCount];
+  uint8_t temperatureCount = 32;
+  float temperature[temperatureCount];
+
+  float tempAverageTemperature = 0.0;
+  float tempAveragePressure = 0.0;
+
+  //This function writes the results of continuous measurements to the arrays given as parameters
+  //The parameters temperatureCount and pressureCount should hold the sizes of the arrays temperature and pressure when the function is called
+  //After the end of the function, temperatureCount and pressureCount hold the numbers of values written to the arrays
+  //Note: The Dps310 cannot save more than 32 results. When its result buffer is full, it won't save any new measurement results
+  int16_t ret = Dps310PressureSensor.getContResults(temperature, temperatureCount, pressure, pressureCount);
+
+  if (ret != 0)
+  {
+    Serial.println();
+    Serial.println();
+    Serial.print("FAIL! ret = ");
+    Serial.println(ret);
+  }
+  else
+  {
+    Serial.println();
+    Serial.println();
+    Serial.print(temperatureCount);
+    Serial.println(" temperature values found: ");
+    for (int16_t i = 0; i < temperatureCount; i++)
+    {
+      Serial.print(temperature[i]);
+      Serial.println(" degrees of Celsius");
+      tempAverageTemperature += temperature[i];
+    }
+
+    Serial.println();
+    Serial.print(pressureCount);
+    Serial.println(" pressure values found: ");
+    for (int16_t i = 0; i < pressureCount; i++)
+    {
+      Serial.print(pressure[i] * PASCAL_TO_MILLIBAR);
+      Serial.println(" mBar");
+      tempAveragePressure += pressure[i] * PASCAL_TO_MILLIBAR;
+    }
+
+    tempAverageTemperature /= temperatureCount;
+    tempAveragePressure /= pressureCount;
+
+    Serial.print("Average Temperature: ");
+    Serial.print(tempAverageTemperature);
+    Serial.println(" *C");
+    Serial.print("Average Pressure: ");
+    Serial.print(tempAveragePressure);
+    Serial.println(" mBar");
+    Serial.print("Loop Counter: ");
+    Serial.println(loopCounter);
+
+    // add these temperary values to the array to be averaged later
+    averageTemperatureArray[loopCounter] = tempAverageTemperature;
+    averagePressureArray[loopCounter] = tempAveragePressure;
+  }
+
+
+
+  //we hit a transmit loop!
+  if (loopCounter >= (NUM_LOOPS - 1))
+  {
+    
+    //Calculate final averages
+    averageTemperature = 0.0;
+    averagePressure = 0.0;
+    for (int i = 0; i < loopCounter; i++)
+    {
+      averageTemperature += averageTemperatureArray[i];
+      averagePressure += averagePressureArray[i];
+      Serial.print("averageTemperatureArray[i]: ");
+      Serial.println(averageTemperatureArray[i]);
+      Serial.print("averageTemperature: ");
+      Serial.println(averageTemperature);
+      
+      Serial.print("averagePressureArray[i]: ");
+      Serial.println(averagePressureArray[i]);
+      Serial.print("averagePressure: ");
+      Serial.println(averagePressure);
+    }
+    averageTemperature /= loopCounter;
+    averagePressure /= loopCounter;
+
+    Serial.print("loopCounter: ");
+    Serial.println(loopCounter);
+
+    Serial.print("Final Average Temperature: ");
+    Serial.print(averageTemperature);
+    Serial.println(" *C");
+    Serial.print("Final Average Pressure: ");
+    Serial.print(averagePressure);
+    Serial.println(" mBar");
+    
+
+    //transmit stuff
+    
+    // ---------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------- InfluxDB ----------------------------------------------------------
+    // ---------------------------------------------------------------------------------------------------------------
+    
+    // Clear fields for reusing the point. Tags will remain untouched
+    sensor.clearFields();
   
-  while (!dps.temperatureAvailable() || !dps.pressureAvailable()) {
-    return; // wait until there's something to read
-  }
-
-  dps.getEvents(&temp_event, &pressure_event);
-  Serial.print(F("Temperature = "));
-  Serial.print(temp_event.temperature);
-  Serial.println(" *C");
-
-  Serial.print(F("Pressure = "));
-  Serial.print(pressure_event.pressure);
-  Serial.println(" hPa"); 
-
-
-  // ---------------------------------------------------------------------------------------------------------------
-  // ------------------------------------------- InfluxDB ----------------------------------------------------------
-  // ---------------------------------------------------------------------------------------------------------------
+    // Store measured value into point
   
-  // Clear fields for reusing the point. Tags will remain untouched
-  sensor.clearFields();
+    //add temperature and pressure info
+    sensor.addField("Temperature", averageTemperature);
+    sensor.addField("Pressure", averagePressure);
+  
+    // Print what are we exactly writing
+    Serial.print("Writing: ");
+    Serial.println(sensor.toLineProtocol());
+  
+    // Check WiFi connection and reconnect if needed
+    if (wifiMulti.run() != WL_CONNECTED) {
+      Serial.println("Wifi connection lost");
+    }
+  
+    // Write point
+    if (!client.writePoint(sensor)) {
+      Serial.print("InfluxDB write failed: ");
+      Serial.println(client.getLastErrorMessage());
+    }
 
-  // Store measured value into point
-  // Report RSSI of currently connected network
-  sensor.addField("rssi", WiFi.RSSI());
+    //reset the loop counter
+    loopCounter = 0u;
 
-  //add temperature and pressure info
-  sensor.addField("Temperature", temp_event.temperature);
-  sensor.addField("Pressure", pressure_event.pressure);
-
-  // Print what are we exactly writing
-  Serial.print("Writing: ");
-  Serial.println(sensor.toLineProtocol());
-
-  // Check WiFi connection and reconnect if needed
-  if (wifiMulti.run() != WL_CONNECTED) {
-    Serial.println("Wifi connection lost");
+    //reset averages and such
+    averageTemperature = 0.0;
+    averagePressure = 0.0;
+  
+    for(int i = 0; i < NUM_LOOPS; i++)
+    {
+      averageTemperatureArray[i] = 0.0;
+      averagePressureArray[i] = 0.0;
+    }
+  }
+  else
+  {
+     //increment the loop counter
+     loopCounter += 1u;
   }
 
-  // Write point
-  if (!client.writePoint(sensor)) {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
 
 
+ 
 
   // ---------------------------------------------------------------------------------------------------------------
   // ------------------------------------------- Loop Delay --------------------------------------------------------
   // ---------------------------------------------------------------------------------------------------------------
-  Serial.println("Wait 10s");
-  delay(10000); //10 seconds
+  Serial.print("Wait ");
+  Serial.print(REST_TIME);
+  Serial.println(" seconds");
+  delay(REST_TIME_MS);
 }
